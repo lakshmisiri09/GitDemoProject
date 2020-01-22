e326dfc714a240174f7947fd715e5da7137f30a3
Harshita <Harshita@192.168.2.11>
2020-01-21 22:44:23 -0500

    First Git Repository

---
+++ src/GitRepoClass.java
@@ -0,0 +1,16 @@
+
+public class GitRepoClass {
+
+	public static void main(String[] args) {
+		String title="gmail";
+		
+		if(title=="gmail")
+		{
+			System.out.println("print title" +title);
+		}
+		else
+			System.out.println("title is not gmail");
+
+	}
+
+}
